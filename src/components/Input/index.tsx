77c51a854b8b27c74604c72008130bbb9e153e9c
Adriano Misina <adriano.misina@agroboard.com.br>
2023-10-14 10:43:00 -0300

    Estilizando o placeholder

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {TextInputProps} from "react-native";
+import { Container } from "./styles";
+import {useTheme} from 'styled-components/native'
+
+export default function Input({...rest}: TextInputProps) {
+  const {COLORS}= useTheme()  
+
+  return (
+    <Container
+    placeholderTextColor={COLORS.GRAY_300}
+    {...rest}
+    />
+  )
+}
--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -3,6 +3,7 @@ import Header from '@components/Header';
 import { Container, Content, Icon } from './styles';
 import Highlight from '@components/Highlight';
 import Button from '@components/Button';
+import Input from '@components/Input';
 
 const NewGroup = () => {
   return (
@@ -15,8 +16,13 @@ const NewGroup = () => {
                     subtitle='crie a turma para adicionar as pessoas'
                 />
 
+                <Input 
+                  placeholder="Nova turma"
+                />
+
                 <Button 
                     title ="Criar"
+                    style={{marginTop: 20}}
                 />
             </Content>
     </Container>
